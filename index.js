13d3f37c122fdc9cb80965a5829af58a48e25105
Muhammed Haque <nohorjo@gmail.com>
2024-12-02 13:46:46 +0100

    add durations

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
+
 const { promises: fs }  = require("fs");
+const cp = require('child_process');
 
 const AUDIO_EXTS = ['mp3', 'opus'];
 const VIDEO_EXTS = ['mkv', 'mp4', 'webm'];
 
+const exec = s => new Promise((res, rej) => cp.exec(s, (err, stdout, stderr) => {
+  if (err) {
+    rej(err);
+  } else {
+    if (stderr) {
+      console.error(stderr);
+    }
+    res(stdout);
+  }
+}));
 
 const app = express();
 
@@ -43,9 +55,9 @@ app.get('/media/*', async function(req, res, next) {
     const contents = await fs.readdir(
       path,
       { withFileTypes: true },
-    )
+    );
 
-    res.send(contents.map(
+    const items = contents.map(
       x => {
         let type;
         if (x.isDirectory() || x.isSymbolicLink()) {
@@ -57,7 +69,20 @@ app.get('/media/*', async function(req, res, next) {
         }
         return { name: x.name, type };
       }
-    ).filter(x => !!x.type && !x.name.startsWith('_')));
+    );
+
+    if (req.query.withTimes === 'true') {
+      for (const item of items) {
+        if (item.type !== 'D') {
+          try {
+            const dur = await exec(`ffprobe "${path}/${item.name}" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal`);
+            item.duration = dur.replace(/\..*\n/, '');
+          } catch (e) {}
+        }
+      }
+    }
+
+    res.send(items.filter(x => !!x.type && !x.name.startsWith('_')));
   } catch (e) {
     res.status(500).send(e);
   }
